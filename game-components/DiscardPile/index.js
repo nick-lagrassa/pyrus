6f9f04e7bf042e41a12eeacc51d3b6f21e106897
Armaan Shah <armaanshah96@gmail.com>
2017-10-17 11:48:46 -0500

    DiscardPile initial implementation

--- game-components/DiscardPile/index.js
+++ game-components/DiscardPile/index.js
@@ -1,5 +1,15 @@
 class DiscardPile {
-    
+    // The DiscardPile class maintains the cards discarded during the game
+    // -> DiscardPile
+    constructor() {
+        this.cards = [];
+    }
+
+    // add a card to List[Card] attribute
+    // Card ->
+    set addCard(card) {
+        this.cards.push(card);
+    }
 }
 
 export default DiscardPile;
