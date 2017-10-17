f43ab85f6146de1eaccaf177c7a6cf9ee16d1913
Josh Shi <thehandsomepanther@gmail.com>
2017-10-17 09:24:34 -0500

    code review for deck

--- game-components/Deck/index.js
+++ game-components/Deck/index.js
@@ -1,24 +1,28 @@
 class Deck {
-    constructor(initialState) {
+    // List[Card] -> Deck
+    constructor(cards) {
         this.endTurnPickupValue = 2;
-        this.currentSet = initialState;
+        this.cards = cards;
     }
 
-    removeOne() {
-        if(emptyDeck())
-          return null;
-        return this.currentSet.pop(0)
+    // Draw n cards from the top of the deck. Default to the number of cards
+    // drawn at the end of the turn
+    // int -> List[Card]
+    draw(n = this.endTurnPickupValue) {
+        let drawnCards = [];
+        for (let i = 0; i < n; i++) {
+            if (this.empty) {
+                return drawnCards;
+            }
+
+            drawnCards.push(this.cards.pop());
+        }
+        return drawnCards;
     }
 
-    removeTwo() {
-        let playerPickup = []
-        for(let i = 0; i < this.endTurnPickupValue; i++) {
-            playerPickup.push(removeOne());
-        return playerPickup;
-    }
-
-    emptyDeck() {
-        return this.currentSet.length == 0;
+    // -> boolean
+    get empty() {
+        return this.cards.length === 0;
     }
 }
 
