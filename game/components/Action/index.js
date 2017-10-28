91d6990464c6b296d893d13e734dbda5f0bcd849
Armaan Shah <armaanshah96@gmail.com>
2017-10-28 16:27:24 -0500

    rules enforcer, implement discard move in is legal action

--- game/components/Action/index.js
+++ game/components/Action/index.js
@@ -1,10 +1,11 @@
 class Action {
     // The Action class is abstract and is implemented by ConsumeAction, WriteAction and Discard Action
     // -> Action
-    constructor() {
+    constructor(player) {
         if (new.target === Action) {
             throw new TypeError("Action is an abstract class.");
         }
+        this.player = player;
     }
 }
 
