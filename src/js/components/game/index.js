132544f302d7d9783cf98eca2b9f52aee39a0fdf
Josh Shi <thehandsomepanther@gmail.com>
2017-10-30 17:27:09 -0500

    set up routes

--- src/js/components/game/index.js
+++ src/js/components/game/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import Card from '../card';
+import { getGame } from '../../lib/api';
 
 export default class Game extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            ready: false
+        };
+    }
+
+    componentWillMount() {
+        const { gameId } = this.props.match.params;
+        getGame(gameId)
+            .then(body => {
+                this.setState({
+                    game: body,
+                    ready: true
+                });
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
-        const { game } = this.props;
+        const { ready, game } = this.state;
+        
+        if (!ready) {
+            return <h1>loading</h1>
+        }
 
         return (
             <div>
