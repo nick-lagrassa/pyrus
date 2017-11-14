a7911066351b163ca0eae40fc4cee7dac1adc58c
Josh Shi <thehandsomepanther@gmail.com>
2017-11-13 20:29:58 -0600

    new way of selecting actions

--- src/js/components/gameRunning/index.js
+++ src/js/components/gameRunning/index.js
@@ -6,12 +6,14 @@ import Hand from '../hand';
 import Prompt from '../../containers/prompt';
 import limitEval from '../../util/limitEval';
 import { myTurn } from '../../../../game/util';
+import { MOVE_DISCARD, MOVE_CONSUME, MOVE_WRITE } from '../../../../game/constants/move';
 
 export default class GameRunning extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            testResults: null
+            testResults: null,
+            moveSelect: null
         };
     }
 
@@ -72,15 +74,53 @@ export default class GameRunning extends Component {
         }
     }
 
+    handleDiscardMoveClick = () => {
+        this.setState({
+            moveSelect: MOVE_DISCARD
+        });
+    }
+
+    handleConsumeMoveClick = () => {
+        this.setState({
+            moveSelect: MOVE_CONSUME
+        });
+    }
+
+    handleCardClick = card => {
+        const { stream } = this.props;
+        const { moveSelect } = this.state;
+
+        switch (moveSelect) {
+            case MOVE_CONSUME:
+                stream.sendAction({
+                    type: moveSelect,
+                    card
+                });
+                break;
+            case MOVE_DISCARD:
+                stream.sendAction({
+                    type: moveSelect,
+                    card
+                });
+                break;
+            default: 
+                return;
+        }
+
+        this.setState({
+            moveSelect: null
+        });
+    }
+
     render() {
         const { me, game, gameId, stream, players } = this.props;
-        const { testResults } = this.state;
+        const { testResults, moveSelect } = this.state;
         let numTestsPassed = testResults ? testResults.filter(result => result.passed).length : null;
 
         return (
             <div className="flex flex-column vh-100 relative overflow-hidden">
                 <InfoHeader />
-                <div className="flex mw9 center mb6">
+                <div className="flex mw8 center mb6">
                     <div className="w-50 pt3 ph3 pb6 aspect-ratio-object overflow-scroll">
                         <Prompt />
                         { testResults &&
@@ -104,30 +144,53 @@ export default class GameRunning extends Component {
                             getEditor={ editor => this.editorElement = editor }
                         />
                     </div>
+                    <div className="absolute right--2 top-4 slide-left flex flex-column">
+                        { myTurn(me, game, players) &&
+                            <div className="flex flex-column">
+                                <p className="f6 silver mv0">ACTIONS</p>
+                                <input
+                                    type="button"
+                                    className="db mv1 input-reset ba bg-pear-blue b--pear-blue pa3 br2 white pointer"
+                                    value="Write Code"
+                                    onClick={ this.handleRunCode }
+                                />
+                                <input
+                                    type="button"
+                                    className="db mv1 input-reset ba bg-pear-purple b--pear-purple pa3 br2 white pointer"
+                                    value="Consume Card"
+                                    onClick={ this.handleConsumeMoveClick }
+                                />
+                                <input
+                                    type="button"
+                                    className="db mv1 input-reset ba bg-pear-yellow b--pear-yellow pa3 br2 near-black pointer"
+                                    value="Discard Card"
+                                    onClick={ this.handleDiscardMoveClick }
+                                />
+                            </div>
+                        }
+                        <p className="f6 silver mt4 mb0">RUN</p>
+                        <input
+                            type="button"
+                            className="db mv1 input-reset ba bg-pear-near-white b--pear-light-gray pa3 br2 silver pointer"
+                            value="Run Code"
+                            onClick={ this.handleRunCode }
+                        />
+                    </div>
                 </div>
+                <div className={`absolute absolute--fill bg-near-black ${ moveSelect ? 'o-60 z-999' : 'o-0 z-0 dn' }`}></div>
                 <div>
-                    <div className="absolute bottom--2 w-50 left-0 z-999 ph2 flex flex-column self-end">
+                    <div className="absolute bottom--2 w-50 left-0 z-9 ph2 flex flex-column self-end">
                         <p className="silver f6 mv2 pa2 br2 dib bg-pear-near-white self-end ba b--pear-light-gray">{ `${ this.getPartner().name }'s hand` }</p>
                         <div className="flex flex-row-reverse">
-                            <Hand
-                                cards={ this.getPartnersHand() }
-                            />
+                            <Hand cards={ this.getPartnersHand() } />
                         </div>
                     </div>
                     <div className="absolute bottom--2 w-50 right-0 z-999 ph2 flex flex-column">
-                        <div className="flex justify-between">
-                            <p className="pear-near-white f6 mv2 pa2 br2 dib bg-pear-blue self-start ba b--pear-blue">Your hand</p>
-                            <input
-                                type="button"
-                                className="input-reset ba bg-pear-green b--pear-green pa3 br2 white pointer"
-                                value="Run Code"
-                                onClick={ this.handleRunCode }
-                            />
-                        </div>
+                        <p className="pear-near-white f6 mv2 pa2 br2 dib bg-pear-blue self-start ba b--pear-blue">Your hand</p>
                         <div className="flex">
                             <Hand
                                 cards={ this.getMyHand() }
-                                stream={ myTurn(me, game, players) ? stream : null }
+                                handleCardClick={ moveSelect ? this.handleCardClick : null }
                             />
                         </div>
                     </div>
